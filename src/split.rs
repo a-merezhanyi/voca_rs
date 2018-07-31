a05164b03356d6ee1d2436d9aeb87753c5f19f47
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-31 22:19:47 +0200

    added "chars" method

--- src/split.rs
+++ src/split.rs
@@ -1,5 +1,13 @@
 //! Splits `subject` into an chuncks according to given rules.
 
+/// Splits `subject` into an array of characters.
+pub fn chars(s: &str) -> Vec<&str> {
+    if s.len() == 0 {
+        return vec![];
+    }
+    s.split_terminator("").skip(1).collect::<Vec<_>>()
+}
+
 /// Splits `subject` into an array of words.
 pub fn words(s: &str) -> Vec<&str> {
     s.split_whitespace().collect::<Vec<_>>()
--- tests/lib.rs
+++ tests/lib.rs
@@ -5,13 +5,27 @@ mod tests {
     use voca_rs::*;
 
     #[test]
+    fn split_to_chars() {
+        assert_eq!(
+            split::chars(&"gravity".to_string()),
+            ["g", "r", "a", "v", "i", "t", "y"]
+        );
+        assert_eq!(split::chars(&"  ".to_string()), [" ", " "]);
+        assert_eq!(split::chars("\n\t"), ["\n", "\t"]);
+    }
+    #[test]
+    #[should_panic]
+    fn split_to_chars_panic() {
+        assert_eq!(split::chars(&"gravity".to_string()), ["g", "r", "a"]);
+    }
+
+    #[test]
     fn split_words() {
         assert_eq!(
             split::words(&"gravity can cross dimensions".to_string()),
             ["gravity", "can", "cross", "dimensions"]
         );
     }
-
     #[test]
     #[should_panic]
     fn split_words_panic() {
