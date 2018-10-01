57a595553993175954634135b7b08affa27aa72a
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-10-02 00:44:45 +0200

    updated a package's version

--- src/utils.rs
+++ src/utils.rs
@@ -9,7 +9,7 @@
 /// utils::VERSION;
 /// // => "0.1.0"
 /// ```
-pub const VERSION: &'static str = "0.1.0";
+pub const VERSION: &'static str = "0.2.0";
 
 /// The concatenation of the `ascii_lowercase` and `ascii_uppercase` constants described below. This value is not locale-dependent.
 ///
