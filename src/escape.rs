e039d34b4ce99c02c513037d408919015b341fac
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-10-19 21:36:58 +0200

    refactoring

--- src/escape.rs
+++ src/escape.rs
@@ -16,17 +16,37 @@ use split;
 pub fn escape_html(subject: &str) -> String {
     match subject.len() {
         0 => "".to_string(),
+        _ => map_replace(&subject, "html"),
+    }
+}
+
+fn map_replace(subject: &str, map_type: &str) -> String {
+    let html_symbols = vec!["<", ">", "&", "'", "\"", "`"];
+    let special_symbols = vec!["&lt;", "&gt;", "&amp;", "&#x27;", "&quot;", "&#x60;"];
+    let mut res = String::new();
+    let key;
+    let mut value = vec![];
+    match map_type {
+        "html" => {
+            key = html_symbols;
+            value = special_symbols
+        }
+        "un_html" => {
+            key = special_symbols;
+            value = html_symbols
+        }
+        _ => key = vec![],
+    }
+    match key.len() {
+        0 => subject.to_string(),
         _ => {
-            let mut res = String::new();
             for c in split::chars(&subject) {
-                match c {
-                    "<" => res.push_str("&lt;"),
-                    ">" => res.push_str("&gt;"),
-                    "&" => res.push_str("&amp;"),
-                    "\"" => res.push_str("&quot;"),
-                    "'" => res.push_str("&#x27;"),
-                    "`" => res.push_str("&#x60;"),
-                    _ => res.push_str(&c),
+                match key.iter().position(|&x| x == c) {
+                    Some(i) => {
+                        println!("c={} i={}, {}", c, i, value[i]);
+                        res.push_str(value[i])
+                    }
+                    _ => res.push_str(c),
                 }
             }
             res
