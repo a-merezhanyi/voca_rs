c6373070142c6d57631ee31271e12d18cd9d1f27
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-11-07 11:30:51 +0100

    const &'static str -> const &str

--- src/utils.rs
+++ src/utils.rs
@@ -9,7 +9,7 @@
 /// utils::VERSION;
 /// // => "1.0.0"
 /// ```
-pub const VERSION: &'static str = "1.0.0";
+pub const VERSION: &str = "1.0.0";
 
 /// The concatenation of the `ascii_lowercase` and `ascii_uppercase` constants described below. This value is not locale-dependent.
 ///
@@ -21,7 +21,7 @@ pub const VERSION: &'static str = "1.0.0";
 /// // => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 /// ```
 // TODO: make a combination of ASCII_LOWERCASE, ASCII_UPPERCASE
-pub const ASCII_LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+pub const ASCII_LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 /// The lowercase letters `"abcdefghijklmnopqrstuvwxyz"`. This value is not locale-dependent and will not change.
 ///
@@ -32,7 +32,7 @@ pub const ASCII_LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM
 /// utils::ASCII_LOWERCASE;
 /// // => "abcdefghijklmnopqrstuvwxyz"
 /// ```
-pub const ASCII_LOWERCASE: &'static str = "abcdefghijklmnopqrstuvwxyz";
+pub const ASCII_LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
 
 /// The uppercase letters `"ABCDEFGHIJKLMNOPQRSTUVWXYZ"`. This value is not locale-dependent and will not change.
 ///
@@ -44,7 +44,7 @@ pub const ASCII_LOWERCASE: &'static str = "abcdefghijklmnopqrstuvwxyz";
 /// // => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 /// ```
 // TODO: make a variation of ASCII_LOWERCASE
-pub const ASCII_UPPERCASE: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+pub const ASCII_UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 /// The string "0123456789".
 ///
@@ -55,7 +55,7 @@ pub const ASCII_UPPERCASE: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 /// utils::DIGITS;
 /// // => "0123456789"
 /// ```
-pub const DIGITS: &'static str = "0123456789";
+pub const DIGITS: &str = "0123456789";
 
 /// The string `"0123456789abcdefABCDEF"`.
 ///
@@ -67,7 +67,7 @@ pub const DIGITS: &'static str = "0123456789";
 /// // => "0123456789abcdefABCDEF"
 /// ```
 // TODO: make a combination of slices of DIGITS, ASCII_LOWERCASE, ASCII_UPPERCASE
-pub const HEXDIGITS: &'static str = "0123456789abcdefABCDEF";
+pub const HEXDIGITS: &str = "0123456789abcdefABCDEF";
 
 /// The string `"01234567"`.
 ///
@@ -79,7 +79,7 @@ pub const HEXDIGITS: &'static str = "0123456789abcdefABCDEF";
 /// // => "01234567"
 /// ```
 // TODO: make a slice of DIGITS
-pub const OCTDIGITS: &'static str = "01234567";
+pub const OCTDIGITS: &str = "01234567";
 
 /// The string `!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~`.
 ///
@@ -90,7 +90,7 @@ pub const OCTDIGITS: &'static str = "01234567";
 /// utils::PUNCTUATION;
 /// // => !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 /// ```
-pub const PUNCTUATION: &'static str = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+pub const PUNCTUATION: &str = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 /// String of ASCII characters which are considered printable. This is a combination of `digits`, `ascii_letters`, `punctuation`, and `whitespace`.
 ///
@@ -102,7 +102,7 @@ pub const PUNCTUATION: &'static str = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 /// // => 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r
 /// ```
 // TODO: make a conbination of DIGITS, ASCII_LETTERS, PUNCTUATION, WHITESPACE
-pub const PRINTABLE: &'static str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r";
+pub const PRINTABLE: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r";
 
 /// The string `" \t\n\r"`.
 ///
@@ -113,4 +113,4 @@ pub const PRINTABLE: &'static str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFG
 /// utils::WHITESPACE;
 /// // => " \t\n\r"
 /// ```
-pub const WHITESPACE: &'static str = " \t\n\r";
+pub const WHITESPACE: &str = " \t\n\r";
