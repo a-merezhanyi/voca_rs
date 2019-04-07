f67c81d6d7ab83a0bad2cdae239fc7c2ac475d49
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-04-07 23:22:09 +0200

    1.5.0 -> 1.6.0

--- src/utils.rs
+++ src/utils.rs
@@ -7,9 +7,9 @@
 /// ```
 /// use voca_rs::*;
 /// utils::VERSION;
-/// // => "1.5.0"
+/// // => "1.6.0"
 /// ```
-pub const VERSION: &str = "1.5.0";
+pub const VERSION: &str = "1.6.0";
 
 /// The concatenation of the `ascii_lowercase` and `ascii_uppercase` constants described below. This value is not locale-dependent.
 ///
