4050b6f7200b62ba9dcfa3e2c3eb618605535855
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-03-21 15:09:33 +0100

    Bump to 1.10.0

--- src/utils.rs
+++ src/utils.rs
@@ -7,9 +7,9 @@
 /// ```
 /// use voca_rs::*;
 /// utils::VERSION;
-/// // => "1.10"
+/// // => "1.10.0"
 /// ```
-pub const VERSION: &str = "1.10";
+pub const VERSION: &str = "1.10.0";
 
 /// The concatenation of the `ascii_lowercase` and `ascii_uppercase` constants described below. This value is not locale-dependent.
 ///
--- tests/unit/utils.rs
+++ tests/unit/utils.rs
@@ -2,7 +2,7 @@
 
 #[test]
 fn version() {
-    assert_eq!(voca_rs::utils::VERSION, "1.10");
+    assert_eq!(voca_rs::utils::VERSION, "1.10.0");
 }
 #[test]
 fn ascii_letters() {
