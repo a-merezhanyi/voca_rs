bc7105dca8d3a443e6e45937931a8ce43bdd28ec
e1r0nd <a.merezhanyi@gmail.com>
2020-09-06 12:17:14 +0200

    Bump the version: 1.11.0 -> 1.12.0

--- src/utils.rs
+++ src/utils.rs
@@ -7,9 +7,9 @@
 /// ```
 /// use voca_rs::*;
 /// utils::VERSION;
-/// // => "1.11.0"
+/// // => "1.12.0"
 /// ```
-pub const VERSION: &str = "1.11.0";
+pub const VERSION: &str = "1.12.0";
 
 /// The concatenation of the `ascii_lowercase` and `ascii_uppercase` constants described below. This value is not locale-dependent.
 ///
